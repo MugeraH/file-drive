73a3a0ba4493ff964cf1457e5401bd7ea2ab9666
hughes <hughes.bwosi@kenya-airways.com>
2024-03-14 02:23:11 +0300

    set up auth with convex and clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,13 @@ import {
   SignedOut,
   SignedIn,
 } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
   const session = useSession();
+  const files = useQuery(api.files.getFiles);
   const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -27,11 +28,15 @@ export default function Home() {
         </SignOutButton>
       </SignedIn>
 
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+
       <Button
         variant="secondary"
         onClick={() => {
           createFile({
-            name: "Fiona Stark",
+            name: "Stewart Little",
           });
         }}
       >
